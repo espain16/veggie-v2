4f3b33ac218c1540c0d477fd66b49905e6f02c4c
Elise Spain <elisespain@gmail.com>
2020-01-30 17:27:51 -0800

    Knex schema for dahsboards created

--- data/migrations/20200130165814_migration.js
+++ data/migrations/20200130165814_migration.js
@@ -1,6 +1,52 @@
 
 exports.up = function(knex) {
+  return knex.schema
+  .createTable('users', tbl => {
+    tbl.increments();
+    tbl
+    .string('username', 128)
+    .unique()
+    .notNullable()
+    tbl
+    .string('password', 40)
+    .unique()
+    .notNullable()
+  })
+
+  .createTable('user_dash', tbl => {
+    tbl.increments();
+    tbl
+    .integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE')
+  })
+
+  .createTable('general_dash', tbl => {
+    tbl.increments();
+    tbl
+    .integer('ft_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('food_truck')
+    .onUpdate()
+    .onDelete()
+    tbl
+    .integer('rest_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('restaurant')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE')
+
+  })
   
+
 };
 
 exports.down = function(knex) {
