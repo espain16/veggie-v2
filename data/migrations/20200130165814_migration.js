f6f5ce028164729b2c18690ac1a054996abf3a9c
Elise Spain <elisespain@gmail.com>
2020-02-01 13:39:43 -0800

    Cleanup file created

--- data/migrations/20200130165814_migration.js
+++ data/migrations/20200130165814_migration.js
@@ -1,96 +1,45 @@
 
-exports.up = function(knex) {
+exports.up = function(knex,) {
   return knex.schema
   .createTable('users', tbl => {
     tbl.increments();
-    tbl
-    .string('username', 128)
-    .unique()
-    .notNullable()
-    tbl
-    .string('password', 40)
-    .unique()
-    .notNullable()
+    tbl.string('username', 128).unique().notNullable()
+    tbl.string('password', 40).unique().notNullable()
   })
 
   .createTable('user_dash', tbl => {
     tbl.increments();
-    tbl
-    .integer('user_id')
-    .unsigned()
-    .notNullable()
-    .references('id')
-    .inTable('users')
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE')
+    tbl.integer('user_id').unsigned().notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
   })
 
   .createTable('general_dash', tbl => {
     tbl.increments();
-    tbl
-    .integer('ft_id')
-    .unsigned()
-    .notNullable()
-    .references('id')
-    .inTable('food_truck')
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE')
-    tbl
-    .integer('rest_id')
-    .unsigned()
-    .notNullable()
-    .references('id')
-    .inTable('restaurant')
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE')
+    tbl.integer('ft_id').unsigned().notNullable().references('id').inTable('food_truck').onUpdate('CASCADE').onDelete('CASCADE')
+    tbl.integer('rest_id').unsigned().notNullable().references('id').inTable('restaurant').onUpdate('CASCADE').onDelete('CASCADE')
 
   })
 
   .createTable('restaurant', tbl => {
     tbl.increments();
-    tbl
-    .string('name')
-    .string('city')
-    .string('type')
+    tbl.string('name', 255).notNullable()
+    tbl.string('city', 255).notNullable()
+    tbl.string('type', 255).notNullable()
     
   })
 
   .createTable('food_truck', tbl => {
     tbl.increments();
-    tbl
-    .string('name')
-    .string('type')
+    tbl.string('name', 255).notNullable()
+    tbl.string('type', 255).notNullable()
   })
 
   .createTable('rating', tbl => {
     tbl.increments();
-    tbl
-    .integer('rating')
-    .string('comment')
-    tbl
-    .integer('user_id')
-    .unsigned()
-    .notNullable()
-    .references('id')
-    .inTable('users')
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE')
-    tbl
-    .integer('ft_id')
-    .unsigned()
-    .notNullable()
-    .references('id')
-    .inTable('food_truck')
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE')
-    tbl
-    .integer('rest_id')
-    .unsigned()
-    .notNullable()
-    .references('id')
-    .inTable('restaurant')
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE')
+    tbl.integer('rating')
+    tbl.string('comment')
+    tbl.integer('user_id').unsigned().notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+    tbl.integer('ft_id').unsigned().notNullable().references('id').inTable('food_truck').onUpdate('CASCADE').onDelete('CASCADE')
+    tbl.integer('rest_id').unsigned().notNullable().references('id').inTable('restaurant').onUpdate('CASCADE').onDelete('CASCADE')
 
   })
   
