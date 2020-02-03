dac318eb9637aae31f2a7ca311918ba3bb8e66b8
Elise Spain <elisespain@gmail.com>
2020-02-02 18:14:25 -0800

    Rating seed file entries complete

--- data/seeds/04-rating.js
+++ data/seeds/04-rating.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('rating').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('rating').insert([
+        {rating: 2, comments:'Lacked flavor, do You guys know what salt is ?', ft_id: 1, user_id: 1,rest_id: 1},
+        {rating: 4, comments:'Best Vegan food I have eaten by far ', ft_id: 2, user_id: 1,rest_id: 4},
+        {rating: 5, comments:'Whoa! Amazing just amazing', ft_id: 4, user_id: 3,rest_id: 7}
       ]);
     });
 };
