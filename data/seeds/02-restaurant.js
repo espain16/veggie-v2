b1b24932e0e2dce5473c373737354b634122dd9b
Elise Spain <elisespain@gmail.com>
2020-02-02 17:36:22 -0800

    Restaurant seed file entries complete

--- data/seeds/02-restaurant.js
+++ data/seeds/02-restaurant.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('restaurant').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('restaurant').insert([
+        {name:'Sliver', city: 'Berkeley', type: 'Vegetarian'},
+        {name:"Butcher's Son", city: 'Berkeley', type: 'Vegan'},
+        {name:'HeyO!', city: 'Mountain View', type: 'Vegan'}
       ]);
     });
 };
+
+//truncate is used instead of del(). to help restart our table id to 1. 
