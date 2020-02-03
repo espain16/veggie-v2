471d73bd47c15d5dd4ba8a448c574571b9071647
Elise Spain <elisespain@gmail.com>
2020-02-02 17:56:46 -0800

    Food_truck seed file entries complete

--- data/seeds/03-food_truck.js
+++ data/seeds/03-food_truck.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('food_truck').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('food_truck').insert([
+        {name: 'RawASF', type: 'Vegan'},
+        {name: 'Vegan Veganos', type: 'Vegan'},
+        {name: 'helping Food Truck', type: 'Vegan'}
       ]);
     });
 };
